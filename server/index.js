aa268b909ccebe65c7f5c4c373b51c196af8e823
jmjles <jmjles@hotmail.com>
2019-02-23 14:34:26 -0800

    Changed Prod Folder for Heroku

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ const port = '5000';
 const clicker = require('./routes/api/clicker-game.js');
 app.use('/api/clicker-game', clicker);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'));
+}
 // Launch Server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
